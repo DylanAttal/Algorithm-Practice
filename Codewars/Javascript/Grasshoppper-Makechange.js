34096349b9eae4715e20a135eff2a157f8c5e8b1
Dylan Attal <41970259+DylanAttal@users.noreply.github.com>
2018-10-30 13:12:58 -0400

    Create Grasshoppper-Makechange.js

---
+++ Codewars/Javascript/Grasshoppper-Makechange.js
@@ -0,0 +1,14 @@
+/* Get change
+You go to the store and have a 10 dollar bill to spend. You buy candy, chips, and soda. Find out how much change you get back from the cashier.
+
+Item	Cost
+Candy	$1.42
+Chips	$2.40
+Soda	$1.00
+Create 5 variables and use the cost from the table above to set their values. */
+
+let money  = 10;
+let candy  = 1.42;
+let chips  = 2.4;
+let soda   = 1;
+let change = 5.18;
