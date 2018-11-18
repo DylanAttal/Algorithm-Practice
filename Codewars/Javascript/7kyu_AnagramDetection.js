ac67b80aeb1fa8019c596e07d538081d18509db0
Dylan Attal <41970259+DylanAttal@users.noreply.github.com>
2018-11-18 17:41:01 -0500

    Create 7kyu_AnagramDetection.js

---
+++ Codewars/Javascript/7kyu_AnagramDetection.js
@@ -0,0 +1,20 @@
+/* An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+Note: anagrams are case insensitive
+
+Complete the function to return true if the two arguments given are anagrams of theeach other; return false otherwise.
+
+Examples
+"foefet" is an anagram of "toffee"
+
+"Buckethead" is an anagram of "DeathCubeK" */
+
+var isAnagram = function(test, original) {
+  let newTest = test.toLowerCase().split('').sort().join('')
+  let newOriginal = original.toLowerCase().split('').sort().join('')
+  if (newTest === newOriginal) {
+    return true
+  } else {
+    return false
+  }
+};
