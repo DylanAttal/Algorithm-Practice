36fe77c1eaf7d3c99a127e4c7ec5282960a89396
Dylan Attal <41970259+DylanAttal@users.noreply.github.com>
2018-11-18 17:39:42 -0500

    Added solution

--- Codewars/Javascript/7kyu_DigitsAverage.js
+++ Codewars/Javascript/7kyu_DigitsAverage.js
@@ -20,3 +20,18 @@ original: 8   9
 
 */
 
+function digitsAverage(input) {
+  if (input < 10) {
+    return input
+  }
+  while (input > 9) {
+    input = String(input)
+    let array = []
+    for (let i = 0; i < input.length - 1; i++) {
+      array.push(Math.round((+input[i] + +input[i + 1]) / 2))
+    }
+    input = +(array.join(''))
+    
+  }
+  return input
+}
