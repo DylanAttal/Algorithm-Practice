be2618b1645f4ec3b6ae996c3e3e2eb31cf61c8a
Dylan Attal <41970259+DylanAttal@users.noreply.github.com>
2018-12-06 14:16:47 -0500

    Adds instructions

--- Codewars/Javascript/7kyu_Coding3MinABC.js
+++ Codewars/Javascript/7kyu_Coding3MinABC.js
@@ -1,7 +1,13 @@
-/*
+/* Task:
+Give you a number array numbers and a number c.
 
+Find out a pair of numbers(we called them number a and number b) from the array numbers, 
+let a*b=c, result format is an array [a,b]
 
-*/
+The array numbers is a sorted array, value range: -100...100
+
+The result will be the first pair of numbers, for example,findAB([1,2,3,4,5,6],6) 
+should return [1,6], instead of [2,3] */
 
 function findAB(numbers,c){
   let answer = []
