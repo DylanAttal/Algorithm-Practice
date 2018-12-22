1424c1a67e43ca4d1d8c156268c4354e931bb183
Dylan Attal <41970259+DylanAttal@users.noreply.github.com>
2018-12-22 13:08:09 -0500

    Creates 8kyu_AdultsOnly.sql

---
+++ Codewars/SQL/8kyu_AdultsOnly.sql
@@ -0,0 +1,9 @@
+/* In your application, there is a section for adults only. You need to get a list of names and ages of users from the users table, who are 18 years old or older.
+
+users table schema
+
+name
+age
+NOTE: Your solution should use pure SQL. Ruby is used within the test cases to do the actual testing. */
+
+SELECT name, age FROM users WHERE age >= 18;
