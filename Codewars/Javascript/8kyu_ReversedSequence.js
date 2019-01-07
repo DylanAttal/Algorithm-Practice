19ed8be049a52f207834c0923b48ef1518edf8f4
Dylan Attal <41970259+DylanAttal@users.noreply.github.com>
2019-01-06 22:25:02 -0500

    Creates 8kyu_ReversedSequence.js

---
+++ Codewars/Javascript/8kyu_ReversedSequence.js
@@ -0,0 +1,11 @@
+/* Get the number n (n>0) to return the reversed sequence from n to 1.
+
+Example : n=5 >> [5,4,3,2,1] */
+
+const reverseSeq = n => {
+  let output = [];
+  for (let i=n; i>0; i--) {
+    output.push(i);
+  }
+  return output;
+}
