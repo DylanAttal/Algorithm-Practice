67fdb76366868d2c38a068514183d58791ce4788
Dylan Attal <41970259+DylanAttal@users.noreply.github.com>
2019-02-28 16:28:23 -0500

    Creates FindersKeepers.js

---
+++ FreeCodeCamp/BasicAlgorithms/FindersKeepers.js
@@ -0,0 +1,10 @@
+/* Create a function that looks through an array (first argument) and returns the first element in the 
+array that passes a truth test (second argument). If no element passes the test, return undefined. */
+
+function findElement(arr, func) {
+  for (let i = 0; i < arr.length; i++) {
+      if (func(arr[i])) {
+        return arr[i]
+      }
+  }
+}
