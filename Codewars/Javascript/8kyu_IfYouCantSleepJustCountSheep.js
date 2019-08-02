37ef9874a15850bf3e9bfa13a5ebe3c31041f92f
Dylan Attal <41970259+DylanAttal@users.noreply.github.com>
2019-08-01 22:49:07 -0400

    Creates 8kyu_IfYouCantSleepJustCountSheep.js

---
+++ Codewars/Javascript/8kyu_IfYouCantSleepJustCountSheep.js
@@ -0,0 +1,11 @@
+/* Task:
+Given a non-negative integer, 3 for example, return a string with a murmur:
+"1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers. */
+
+var countSheep = function (num) {
+  let sheepCount = ''
+  for (let i = 1; i <= num; i++) {
+    sheepCount += i + ' sheep...'
+  }
+  return sheepCount
+}
