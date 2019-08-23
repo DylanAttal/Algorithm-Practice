f8a5fe8dd28cdf86b08668e3d6aea748b7b7e82c
Dylan Attal <41970259+DylanAttal@users.noreply.github.com>
2019-08-23 13:38:42 -0400

    Creates 7kyu_RemoveDuplicateWords.rb

---
+++ Codewars/Ruby/7kyu_RemoveDuplicateWords.rb
@@ -0,0 +1,15 @@
+# Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+
+# Example:
+
+# Input:
+
+# 'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+
+# Output:
+
+# 'alpha beta gamma delta'
+
+def remove_duplicate_words(s)
+  s.split.uniq.join(' ')
+end
