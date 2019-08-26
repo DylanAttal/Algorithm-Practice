619e48e3e7966fbb615985d0bbe01ca7129208d5
Dylan Attal <41970259+DylanAttal@users.noreply.github.com>
2019-08-26 09:45:58 -0400

    Creates 7kyu_NumbersInStrings.rb

---
+++ Codewars/Ruby/7kyu_NumbersInStrings.rb
@@ -0,0 +1,8 @@
+# In this Kata, you will be given a string that has lowercase letters and numbers.
+# Your task is to compare the number groupings and return the largest number.
+
+# For example, solve("gh12cdy695m1") = 695, because this is the largest of all number groupings.
+
+def solve (s)
+  s.scan(/\d+/).map{ |num| num.to_i}.max
+end
