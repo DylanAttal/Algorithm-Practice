47c470e1fb7f4d3ff78db7d35d5508896b8a57a7
Dylan Attal <41970259+DylanAttal@users.noreply.github.com>
2019-11-21 16:47:23 -0500

    Creates 7kyu_RemoveAnchorFromUrl.js

---
+++ Codewars/Javascript/7kyu_RemoveAnchorFromUrl.js
@@ -0,0 +1,12 @@
+/* Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+Examples
+// returns 'www.codewars.com'
+removeUrlAnchor('www.codewars.com#about')
+
+// returns 'www.codewars.com?page=1' 
+removeUrlAnchor('www.codewars.com?page=1') */
+
+function removeUrlAnchor(url) {
+  return url.replace(/#.*/, '')
+}
