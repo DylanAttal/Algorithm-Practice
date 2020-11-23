c40e19a5d1fc35bf7508c160d992f662c93deec1
Dylan Attal <41970259+DylanAttal@users.noreply.github.com>
2020-11-22 21:44:29 -0500

    Creates 8kyu_GetPlanetNameById.js

---
+++ Codewars/Javascript/8kyu_GetPlanetNameById.js
@@ -0,0 +1,50 @@
+/* The function is not returning the correct values. Can you figure out why?
+
+getPlanetName(3); // should return 'Earth'
+
+function getPlanetName(id){
+  var name;
+  switch(id){
+    case 1:
+      name = 'Mercury'
+    case 2:
+      name = 'Venus'
+    case 3:
+      name = 'Earth'
+    case 4:
+      name = 'Mars'
+    case 5:
+      name = 'Jupiter'
+    case 6:
+      name = 'Saturn'
+    case 7:
+      name = 'Uranus'
+    case 8:
+      name = 'Neptune'
+  }
+  
+  return name;
+} */
+
+function getPlanetName(id) {  
+  switch(id) {
+    case 1:
+      return "Mercury";
+    case 2:
+      return "Venus";
+    case 3:
+      return "Earth";
+    case 4:
+      return "Mars";
+    case 5:
+      return "Jupiter";
+    case 6:
+      return "Saturn";
+    case 7:
+      return "Uranus";
+    case 8:
+      return "Neptune";
+    default:
+      return "Invalid planet id";
+  }
+}
