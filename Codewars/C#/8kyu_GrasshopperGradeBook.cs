b8cac2ed704cd70c1518f22d616bce3233c9e3f4
Dylan Attal <41970259+DylanAttal@users.noreply.github.com>
2021-01-25 17:25:02 -0500

    Creates 8kyu_GrasshopperGradeBook.cs

---
+++ Codewars/C#/8kyu_GrasshopperGradeBook.cs
@@ -0,0 +1,46 @@
+/* 
+Grade book
+
+Complete the function so that it finds the mean of the three scores passed to it and returns the letter value associated with that grade.
+
+Numerical Score	Letter Grade
+90 <= score <= 100	'A'
+80 <= score < 90	'B'
+70 <= score < 80	'C'
+60 <= score < 70	'D'
+0 <= score < 60	'F'
+
+Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+*/
+
+using System;
+
+public class Kata
+{
+  public static char GetGrade(int s1, int s2, int s3)
+  {
+    var mean = (s1 + s2 + s3) / 3;
+    if (mean < 60) 
+    {
+      return 'F';
+    }
+    if (mean < 70) 
+    {
+      return 'D';
+    }
+    if (mean < 80) 
+    {
+      return 'C';
+    }
+    if (mean < 90) 
+    {
+      return 'B';
+    }
+    if (mean < 100) 
+    {
+      return 'A';
+    }
+    
+    return 'A';
+  }
+}
