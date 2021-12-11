65cefa322904eaf08440fe6ffe24d87f7532933f
Dylan Attal <41970259+DylanAttal@users.noreply.github.com>
2021-12-11 15:47:00 -0500

    8kyu_ArraySecond.rb

---
+++ Codewars/Ruby/8kyu_ArraySecond.rb
@@ -0,0 +1,14 @@
+# Define a new instance method on the Array class called second, which returns the second item
+# in an array (similar to the way .first and .last work in Ruby).
+
+# If there is no element with index 1 in the array, return nil.
+
+# Examples
+# [3, 4, 5].second  #  => 4 
+# [].second         #  => nil
+
+class Array
+  def second()
+    self[1]
+  end
+end
