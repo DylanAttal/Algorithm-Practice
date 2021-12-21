c018c6650ae7acf7e6230a2f3a6993be5fe64c82
Dylan Attal <41970259+DylanAttal@users.noreply.github.com>
2021-12-21 16:54:03 -0500

    Create FindAllActiveStudents.sql

---
+++ Codewars/SQL/FindAllActiveStudents.sql
@@ -0,0 +1,14 @@
+/* Create a simple SELECT query to display student information of all ACTIVE students.
+
+TABLE STRUCTURE:
+
+students
+Id	FirstName	LastName	IsActive 
+
+Note: IsActive (true or false)
+*/
+
+
+SELECT *
+FROM students
+WHERE IsActive = 1;
