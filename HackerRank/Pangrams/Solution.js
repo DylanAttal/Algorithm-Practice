2295df535a3c79387b1225f57319228f56a05c83
Dylan Attal <41970259+DylanAttal@users.noreply.github.com>
2022-01-05 15:17:46 -0500

    Create Solution.js

---
+++ HackerRank/Pangrams/Solution.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'pangrams' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function pangrams(s) {
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+    'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+    'w', 'x', 'y', 'z'];
+    
+    for (let i = 0; i < alphabet.length; i++) {
+        if (!s.toLowerCase().includes(alphabet[i])) {
+            return 'not pangram';
+        }
+    }
+    
+    return 'pangram';
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const result = pangrams(s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
