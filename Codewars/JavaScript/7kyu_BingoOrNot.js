0b5ad2e9d48fe2d57ed2182f03f7d0bb43c3380a
Dylan Attal <41970259+DylanAttal@users.noreply.github.com>
2022-05-05 12:57:53 -0400

    Create 7kyu_BingoOrNot.js

---
+++ Codewars/JavaScript/7kyu_BingoOrNot.js
@@ -0,0 +1,16 @@
+/*
+For this game of BINGO, you will receive a single array of 10 numbers from 1 to 26 as an input. Duplicate numbers within the array are possible.
+
+Each number corresponds to their alphabetical order letter (e.g. 1 = A. 2 = B, etc). Write a function where you will win the game
+if your numbers can spell "BINGO". They do not need to be in the right order in the input array). Otherwise you will lose.
+
+Your outputs should be "WIN" or "LOSE" respectively.
+*/
+
+function bingo(a) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  const letters = a.map(num => alphabet[num - 1]);
+  const doesHaveBingo = letters.includes('b') && letters.includes('i') &&
+    letters.includes('n') && letters.includes('g') && letters.includes('o');
+  return doesHaveBingo ? "WIN" : "LOSE";
+}
