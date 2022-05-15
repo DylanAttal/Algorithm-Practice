442e7fa2c51b06831a034a1b083825e3009608a3
Dylan Attal <41970259+DylanAttal@users.noreply.github.com>
2022-05-15 10:26:30 -0400

    Create 7kyu_Penultimate.py

---
+++ Codewars/Python/7kyu_Penultimate.py
@@ -0,0 +1,10 @@
+# Find the second-to-last element of a list.
+
+# Example:
+
+# penultimate([1,2,3,4])            # => 3
+# penultimate("Python is dynamic") # => 'i'
+# (courtesy of [haskell.org](http://www.haskell.org/haskellwiki/99_questions/1_to_10))
+
+def penultimate(a):
+  return a[-2]
