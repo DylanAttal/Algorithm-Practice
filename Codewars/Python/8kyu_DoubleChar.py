cbec05f8bec7ab66781a8e6d3cc79996c08ad3bc
Dylan Attal <41970259+DylanAttal@users.noreply.github.com>
2022-06-06 15:36:44 -0400

    Create 8kyu_DoubleChar.py

---
+++ Codewars/Python/8kyu_DoubleChar.py
@@ -0,0 +1,9 @@
+# Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+# Examples (Input -> Output):
+# * "String"      -> "SSttrriinngg"
+# * "Hello World" -> "HHeelllloo  WWoorrlldd"
+# * "1234!_ "     -> "11223344!!__  "
+
+def double_char(s):
+    return ''.join(letter * 2 for letter in s)
