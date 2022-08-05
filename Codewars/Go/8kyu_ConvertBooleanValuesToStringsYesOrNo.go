444312eee76e57c8ebae1a9e7b216463a2992ace
Dylan Attal <41970259+DylanAttal@users.noreply.github.com>
2022-08-05 14:43:58 -0400

    Create 8kyu_ConvertBooleanValuesToStringsYesOrNo.go

---
+++ Codewars/Go/8kyu_ConvertBooleanValuesToStringsYesOrNo.go
@@ -0,0 +1,11 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+package kata
+
+func BoolToWord(word bool) string {
+  if (word) {
+    return "Yes";
+  }
+  
+  return "No";
+}
