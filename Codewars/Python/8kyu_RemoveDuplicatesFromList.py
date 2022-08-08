9e8b07525795ded4bb46a257bdd631fd5f6e0258
Dylan Attal <41970259+DylanAttal@users.noreply.github.com>
2022-08-08 13:24:53 -0400

    Create 8kyu_RemoveDuplicatesFromList.py

---
+++ Codewars/Python/8kyu_RemoveDuplicatesFromList.py
@@ -0,0 +1,6 @@
+# Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+# The order of the sequence has to stay the same.
+
+def distinct(seq):
+    return list(dict.fromkeys(seq))
