80bae15058a390a8982b69dfc1c438f387f64a71
Dylan Attal <41970259+DylanAttal@users.noreply.github.com>
2022-08-15 17:45:58 -0400

    Create 7kyu_BeginnerSeries3SumOfNumbers.py

---
+++ Codewars/Python/7kyu_BeginnerSeries3SumOfNumbers.py
@@ -0,0 +1,20 @@
+# Given two integers a and b, which can be positive or negative, find the sum of all the
+# integers between and including them and return it. If the two numbers are equal return a or b.
+
+# Note: a and b are not ordered!
+
+# Examples (a, b) --> output (explanation)
+# (1, 0) --> 1 (1 + 0 = 1)
+# (1, 2) --> 3 (1 + 2 = 3)
+# (0, 1) --> 1 (0 + 1 = 1)
+# (1, 1) --> 1 (1 since both are same)
+# (-1, 0) --> -1 (-1 + 0 = -1)
+# (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+def get_sum(a, b):
+    if a == b:
+        return a
+    elif a < b:
+        return sum(range(a, b + 1))
+    else:
+        return sum(range(b, a + 1))
