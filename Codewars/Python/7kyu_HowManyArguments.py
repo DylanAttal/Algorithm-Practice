e92dc2000980f68eba9f905c2bf2c2e5062f6297
Dylan Attal <41970259+DylanAttal@users.noreply.github.com>
2022-11-28 22:26:22 -0500

    Create 7kyu_HowManyArguments.py

---
+++ Codewars/Python/7kyu_HowManyArguments.py
@@ -0,0 +1,7 @@
+# Create a function args_count, that returns the count of passed arguments
+
+# args_count(1, 2, 3) -> 3
+# args_count(1, 2, 3, 10) -> 4
+
+def args_count(*args, **kwargs):
+    return len(args) + len(kwargs)
